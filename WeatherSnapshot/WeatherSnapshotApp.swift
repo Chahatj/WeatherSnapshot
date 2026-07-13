a241c5026593241e146e0e12679489a2bc0e25cf
chahatjain <chahat.jain@telekom-digital.com>
2026-07-13 21:55:14 +0530

    Initial Commit

---
+++ WeatherSnapshot/WeatherSnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSnapshotApp.swift
+//  WeatherSnapshot
+//
+//  Created by Chahat Jain on 13/07/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
